5402f379d7c325e877941c24f6f13e3f217e1c55
Luiz Lamyanba <spikyluiz@gmail.com>
2026-01-18 12:09:27 +0530

    update ec2.py

--- Phase-1-[Setup and basics]/AWS fundamentals/EC2/EC2 boto3/ec2_regions.py
+++ Phase-1-[Setup and basics]/AWS fundamentals/EC2/EC2 boto3/ec2_regions.py
@@ -1,6 +1,6 @@
-import boto3
+import boto3  
 
-ec2 = boto3.client('ec2')
+ec2 = boto3.client('ec2')    
 regions = ec2.describe_regions()
 for region in regions['Regions']:
     print(region['RegionName'])
